fd42f1c870a828ec77c6872b60914833471e7558
dazzle <7512280+alexanderanter@users.noreply.github.com>
2018-07-30 11:13:48 +0200

    updated description

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import Footer from './components/footer';
 export default props => (
   <div>
     <Header
-      title="Swedish Tech Events"
-      subtitle="Events for developers, technologists, and other geeks in Sweden"
+      title="Decentralized Blockchain Events"
+      subtitle="CryptoCurrency events about Blockchain, Bitcoin and Ethereum around the world."
     />
     <main className="section">
       <div className="container">
@@ -17,7 +17,7 @@ export default props => (
           <a href="https://www.eventbrite.com/">eventbrite.com</a> that is
           categorized with tech and from our GitHub every hour.{' '}
           <a
-            href="https://github.com/swedishtechevents/events/issues/new"
+            href="https://github.com/reftoken/devents-events/issues/new"
             rel="noopener noreferrer"
             target="_blank"
           >
